8f84e50e290c8747813b2327fd34d924ef1006f5
harisantoso <harysanttos@gmail.com>
2022-03-17 21:53:41 +0700

    belajar tipe data, konversi dan function

---
+++ introduction/script/app.js
@@ -0,0 +1,19 @@
+// tipe data string
+const paketKursus = 'Java Script'
+
+// tipe data number
+const hargaKursus = 100000
+
+// tipe data boolean
+const tipeBoolean = true
+
+console.log(typeof tipeBoolean)
+
+// tipe data array
+const kursus = ['javascript', 'php', 'swif']
+
+// memanggil dengan console
+console.log(kursus[1])
+
+// memanggil dengan dom
+document.getElementById("array").innerHTML = kursus[2]
---
+++ introduction/script/function-parameter-script.js
@@ -0,0 +1,11 @@
+function hallo() {
+  nama = prompt("masukan nama anada!!")
+  alert("Halo " + nama)
+}
+hallo()
+
+function email() {
+  email = prompt("masukan email anada")
+  alert("selama datang diwebsite saya")
+}
+email()
